7abe2d91511a5c7918135847f3f8eb9431bd114b
João Paulo <142356941+felpera32@users.noreply.github.com>
2025-07-09 12:54:01 -0300

    Update trocasimulada.php

--- api/trocasimulada.php
+++ api/trocasimulada.php
@@ -52,6 +52,8 @@ if (isset($_GET['success'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Troca Simulada</title>
     <link rel="stylesheet" href="css/trade.css">
+    <link rel="stylesheet" href="css/trocasimulada.css">
+
 
 </head>
 <body>
